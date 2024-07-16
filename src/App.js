cd5900df527c787e351d13163257674b46fa2cbb
SUNIL GANESH K <139485838+codeswithganesh@users.noreply.github.com>
2024-07-16 09:48:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
 
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <calculator />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import React from 'react';
 import Calculator from './Components/calculator.js';
